0d323822d0f3321984ba708566e472b426c1fbe7
RafaelScbo17 <silverrafael1702@gmail.com>
2024-10-30 16:50:57 -0300

    Página dos doces

--- scripts/doces/preco.js
+++ scripts/doces/preco.js
@@ -1,9 +1,26 @@
 function calcularPreco(){
+    
 
-    const precoString = document.querySelector(".preco");
-    const precoUnitario = parseFloat(precoString);
-    const quantidade = document.querySelector("#quantidade").value;
-    const total = precoUnitario * quantidade;
-    console.log(precoUnitario)
-    document.querySelector("#precoTotal").textContent = `R$ ${total.toFixed(2)}`;
+   const selecionado = document.querySelector('input[name="escolha-doce"]:checked');
+
+   const precoUnitario = parseFloat(selecionado.getAttribute('data-preco'));
+
+   const quantidade = parseInt(document.querySelector("#quantidade").value) || 0;
+
+   const total = precoUnitario * quantidade;
+
+   document.querySelector("#precoTotal").textContent = `R$ ${total.toFixed(2)}`
+   
+
 }
+
+
+function redefinirTotal(){
+    document.querySelector('#precoTotal').textContent = "R$ 0,00";
+    document.querySelector("#quantidade").value = "";
+}
+
+const inputsRadio = document.querySelectorAll('input[name="escolha-doce"]');
+inputsRadio.forEach(input => {
+  input.addEventListener("change", redefinirTotal);
+})
